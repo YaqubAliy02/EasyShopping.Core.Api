84a77ada635dfea2241620f169358fae0511d742
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-21 13:02:54 +0500

    DATA: Add Comment Model

---
+++ Domain/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid UserId { get; set; }
+        public int Rating { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
