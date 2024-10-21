a2fa7a7300a11a71035d2bec0471fca18e38c34f
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-21 12:37:49 +0500

    DATA: Add Order Model

---
+++ Domain/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string OrderStatus { get; set; }
+        public string PaymentStatus { get; set; }
+        public DateTimeOffset DeliveryDate { get; set; }
+        public bool ReceivedStatus { get; set; } = false;
+    }
+}
