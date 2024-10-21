0614ac59a3a23b9b246c56adcdcbd5dd3f394892
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-21 12:44:39 +0500

    DATA: Add Payment Model

---
+++ Domain/Models/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public DateTimeOffset PaymentDate { get; set; }
+        public string PaymentMethod { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
