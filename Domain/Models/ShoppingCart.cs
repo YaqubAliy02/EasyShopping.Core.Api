c8f550ca4a627e46cd3904336e022e7f87bc7293
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-21 12:29:06 +0500

    DATA: Add ShoppingCart Model

---
+++ Domain/Models/ShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models
+{
+    public class ShoppingCart
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
