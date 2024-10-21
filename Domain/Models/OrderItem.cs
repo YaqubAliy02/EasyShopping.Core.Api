a0cbd0cb8bd5e52e2d0a7e0d89049da590030481
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-21 12:40:37 +0500

    DATA: Add OrderItem Model

---
+++ Domain/Models/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
