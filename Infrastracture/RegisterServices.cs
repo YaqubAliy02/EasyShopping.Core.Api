a3c730826b9fd9299b37354b0f4be361eb38907e
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-25 15:47:56 +0500

    SERVICES: Add RegisterService For Infrastructure

---
+++ Infrastracture/RegisterServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastracture
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
