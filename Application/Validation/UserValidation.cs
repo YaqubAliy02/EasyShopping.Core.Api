b6c51ed3dd0da8a376f67e1d0e425e31e2fbf041
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-10-29 11:48:11 +0500

    VALIDATION: Add User Validation

---
+++ Application/Validation/UserValidation.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Application.Validation
+{
+    public class UserValidation : AbstractValidator<User>
+    {
+        public UserValidation()
+        {
+            RuleFor(user => user.UserName).NotEmpty().WithMessage("UserName is required!");
+            RuleFor(user => user.UserRoles).NotEmpty().WithMessage($"{nameof(User.UserRoles)}");
+            RuleFor(user => user.Email).NotEmpty().EmailAddress().WithMessage("Please provide a valid email address in the format: 'example@example.com'. ");
+        }
+    }
+}
