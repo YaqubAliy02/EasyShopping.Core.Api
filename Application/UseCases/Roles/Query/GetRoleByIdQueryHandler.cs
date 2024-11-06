8fa559fe22cf85a693121e438941e742f8a7a7d4
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-11-06 12:00:03 +0500

    MEDIATR: Add GetRoleByIdQueryHandler

---
+++ Application/UseCases/Roles/Query/GetRoleByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Repository;
+using Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.UseCases.Roles.Query
+{
+    public class GetRoleByIdQuery : IRequest<IActionResult> 
+    {
+        public Guid RoleId { get; set; }
+    }
+    public class GetRoleByIdQueryHandler : IRequestHandler<GetRoleByIdQuery, IActionResult>
+    {
+        private readonly IUserRoleRepository userRoleRepository;
+
+        public GetRoleByIdQueryHandler(IUserRoleRepository userRoleRepository)
+        {
+            this.userRoleRepository = userRoleRepository;
+        }
+
+        public async Task<IActionResult> Handle(GetRoleByIdQuery request, CancellationToken cancellationToken)
+        {
+            UserRole userRole = await this.userRoleRepository.GetByIdAsync(request.RoleId);
+
+            if (userRole == null)
+                return new NotFoundObjectResult($"User Role Id: {request.RoleId} is not found");
+
+            return new OkObjectResult(userRole);
+        }
+    }
+}
