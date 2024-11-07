4f79d8d928cf94d1e6a20ac7bc892491eb5b6539
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-11-07 14:26:55 +0500

    MEDIATR: Implement UpdateUserCommandHandler

---
+++ Application/UseCases/Users/Command/UpdateUserCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Application.Repository;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Domain.Models;
+using Application.DTOs.User;
+
+namespace Application.UseCases.Users.Command
+{
+    public class UpdateUserCommand : IRequest<IActionResult>
+    {
+        public string UserName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [PasswordPropertyText]
+        public string Password { get; set; }
+        public Guid[] RolesId { get; set; }
+    }
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, IActionResult>
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+
+        public UpdateUserCommandHandler(
+            IUserRepository userRepository, 
+            IMapper mapper)
+        {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> Handle(UpdateUserCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var user =  this.mapper.Map<User>(request);
+            user = await this.userRepository.UpdateAsync(user);
+
+            if (user is null) return new BadRequestObjectResult(request);
+
+            UserGetDto userGetDto = this.mapper.Map<UserGetDto>(user);
+
+            return new OkObjectResult(userGetDto);
+        }
+    }
+}
