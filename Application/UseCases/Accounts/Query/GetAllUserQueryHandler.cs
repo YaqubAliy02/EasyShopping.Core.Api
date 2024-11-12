75070524c8dde30246c920a7243dc30c404223f7
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-11-12 14:52:02 +0500

    MEDIATR: Add GetAllQueryHandler

---
+++ Application/UseCases/Accounts/Query/GetAllUserQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Application.DTOs.Users;
+using Application.Repository;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.UseCases.Accounts.Query
+{
+    public class GetAllUserQuery : IRequest<IActionResult> { }
+    public class GetAllUserQueryHandler : IRequestHandler<GetAllUserQuery, IActionResult>
+    {
+        private readonly IMapper mapper;
+        private readonly IUserRepository userRepository;
+
+        public GetAllUserQueryHandler(
+            IMapper mapper,
+            IUserRepository userRepository)
+        {
+            this.mapper = mapper;
+            this.userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
+        {
+            var users = await this.userRepository.GetAsync(x => true);
+
+            var resultUser = this.mapper.Map<UserGetDto>(users.AsEnumerable());
+
+            return new OkObjectResult(resultUser);
+        }
+    }
+}
