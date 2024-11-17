d9ba8f183e8663f1a40011ca89f94255c66a94ab
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-11-17 18:35:36 +0500

    SERVICES: Implement ProductRepository Service

---
+++ Application/Repository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace Application.Repository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<List<Product>> GetAllProductsByIdAsync(IEnumerable<Guid> productId);
+    }
+}
---
+++ Infrastracture/Services/ProductRepository.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq.Expressions;
+using Application.Abstraction;
+using Application.Repository;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastracture.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IEasyShoppingDbContext easyShoppingDbContext;
+
+        public ProductRepository(IEasyShoppingDbContext easyShoppingDbContext)
+        {
+            this.easyShoppingDbContext = easyShoppingDbContext;
+        }
+
+        public async Task<Product> AddAsync(Product product)
+        {
+           await this.easyShoppingDbContext.Products.AddAsync(product);
+            int result = await this.easyShoppingDbContext.SaveChangesAsync();
+
+            if(result > 0) return product;
+
+            return null;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+           var product = await this.easyShoppingDbContext.Products.FindAsync(id);
+
+            if(product is not null)
+                this.easyShoppingDbContext.Products.Remove(product);
+
+            int result = await this.easyShoppingDbContext.SaveChangesAsync();
+
+            if(result > 0) return true;
+
+            return false;
+        }
+
+        public async Task<List<Product>> GetAllProductsByIdAsync(IEnumerable<Guid> productId)
+        {
+            return await this.easyShoppingDbContext.Products
+                .Where(p => productId.Contains(p.Id))
+                .ToListAsync();
+        }
+
+        public async Task<IQueryable<Product>> GetAsync(Expression<Func<Product, bool>> expression)
+        {
+            return this.easyShoppingDbContext.Products
+                 .Where(expression)
+                 .Include(p => p.Category)
+                 .Include(p => p.Comments)
+                 .Include(p => p.ShoppingCart)
+                 .Include(p => p.OrderItems)
+                 .Include(p => p.User);
+        }
+
+        public async Task<Product> GetByIdAsync(Guid id)
+        {
+            return await this.easyShoppingDbContext.Products
+           .Include(p => p.Category)
+           .Include(p => p.Comments)
+           .Include(p => p.ShoppingCart)
+           .Include(p => p.OrderItems)
+           .Include(p => p.User)
+           .FirstOrDefaultAsync();
+        }
+
+        public async Task<Product> UpdateAsync(Product updateProduct)
+        {
+            this.easyShoppingDbContext.Products.Update(updateProduct);
+            int result = await this.easyShoppingDbContext.SaveChangesAsync();
+
+            if (result > 0) return updateProduct;
+
+            return null;
+        }
+    }
+}
