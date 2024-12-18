df20b76b223afbfd71c81ccd32f49ea064d65c99
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-12-18 11:30:23 +0500

    SERVICES: Implement SubComment AddAsync Service

---
+++ Infrastracture/Services/SubCommentRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using Application.Abstraction;
+using Application.Repository;
+using Domain.Models;
+
+namespace Infrastracture.Services
+{
+    public class SubCommentRepository : ISubCommentRepository
+    {
+        private readonly IEasyShoppingDbContext easyShoppingDbContext;
+
+        public SubCommentRepository(IEasyShoppingDbContext easyShoppingDbContext)
+        {
+            this.easyShoppingDbContext = easyShoppingDbContext;
+        }
+
+        public async Task<SubComment> AddAsync(SubComment subComment)
+        {
+            await this.easyShoppingDbContext.SubComments.AddAsync(subComment);
+            int result = await this.easyShoppingDbContext.SaveChangesAsync();
+
+            if(result > 0) return subComment;
+
+            return null;
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<SubComment>> GetAsync(Expression<Func<SubComment, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SubComment> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SubComment> UpdateAsync(SubComment entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
